1532ae71a1b04ea0e53da2be7d1beca264997b89
Ben <code@benhunter.me>
2018-02-25 15:30:58 +0900

    powershell

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,4 +1,8 @@
 # Powershell
 
 # Check last reboot time
+# Better option 1 using CIM:
 Get-CimInstance -ClassName win32_operatingsystem | select csname, lastbootuptime
+
+# Option 2 using WMI:
+Get-WmiObject win32_operatingsystem | select csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
