b11c8def4c6162cbe3506e5e9752f62c0a751dc0
Ben <benjaminhunter@gmail.com>
2019-01-27 23:50:33 +0900

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,5 +1,13 @@
 # Powershell
 
+# Equivalent to which in linux
+# https://stackoverflow.com/questions/63805/equivalent-of-nix-which-command-in-powershell
+Get-Command python
+
+# Make an alias and store it in user profile
+New-Alias which get-command
+"`nNew-Alias which get-command" | add-content $profile
+
 # Check last reboot time
 # Better option 1 using CIM:
 Get-CimInstance -ClassName win32_operatingsystem | select csname, lastbootuptime
