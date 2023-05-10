20c77c3002a818f4bbe05e080a3ea87a2a4975fe
Ben Hunter <code@benhunter.me>
2023-05-09 23:55:06 -0500

    Add Netshoot commands

--- cloudy.sh
+++ cloudy.sh
@@ -16,3 +16,19 @@ kubectl --namespace default port-forward svc/gitea-http 3000:3000
 # Containers
 docker push localhost:30000/ben/busybox
 skopeo copy docker://localhost:30000/ben/busybox oci:busybox:latest
+
+# Netshoot Container https://github.com/nicolaka/netshoot
+docker login localhost:30000
+docker build -t ben/netshoot .
+docker tag ben/netshoot localhost:30000/ben/netshoot
+docker push localhost:30000/ben/netshoot
+
+docker build --platform linux/amd64 -t localhost:30000/ben/netshoot:latest-amd64 .
+docker push localhost:30000/ben/netshoot:latest-amd64
+
+# Netshoot Container in K8s
+kubectl run --rm -i --tty temp-busybox --image=localhost:30000/ben/netshoot --restart=Never --overrides='{"spec": {"hostNetwork": true}}'
+
+## In container
+skopeo login --tls-verify=false http://localhost:30000
+skopeo copy --src-tls-verify=false docker://localhost:30000/ben/netshoot oci:netshoot:latest
